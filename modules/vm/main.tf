3bff8efd8fb7bc3e21468a246b3c0642909d5c22
skommana04 <skommana04@gmail.com>
2026-02-16 17:40:42 -0600

    a

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "var.subnet_id"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
   }
 }
