717d83ee18be550d4bf9513654d508eaf7b13cdb
skommana04 <skommana04@gmail.com>
2026-02-16 15:40:17 -0600

    a

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -8,29 +8,30 @@
 
 } */
 resource "azurerm_network_security_group" "ec2_sg" {
-  name                =   var.sg_name #"medha-security-group"
-  location            = azurerm_resource_group.rg_name.location
-  resource_group_name = azurerm_resource_group.rg_name.name
-  dynamic security_rule {
+  name                = var.sg_name #"medha-security-group"
+  location            = var.location
+  resource_group_name = var.rg_name
+ 
+   dynamic "security_rule" {
     for_each = var.network_rules
-    content{
-    name                       = security_rule.key
-    priority                   = security_rule.value.priority
-    destination_port_range     = security_rule.value.port
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    content {
+      name                       = security_rule.key
+      priority                   = security_rule.value.priority
+      destination_port_range     = security_rule.value.port
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
     }
- }
+  }
 }
 
 resource "azurerm_network_security_group" "private_subnet_nsg" {
   name                = var.private_subnet_nsg_name #"medha-security-group"
-  location            = azurerm_resource_group.rg_name.location
-  resource_group_name = azurerm_resource_group.rg_name.name
+  location            = var.location
+  resource_group_name = var.rg_name
 
   security_rule {
     name                       = "allow-from-public-subnet"
@@ -40,7 +41,7 @@ resource "azurerm_network_security_group" "private_subnet_nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefixes     = var.public_subnet_address_prefixes
+    source_address_prefixes    = var.public_subnet_address_prefixes
     destination_address_prefix = "*"
   }
 }
@@ -49,22 +50,22 @@ resource "azurerm_network_security_group" "private_subnet_nsg" {
 
 resource "azurerm_virtual_network" "medha_network" {
   name                = var.network_name #"medha-network"
-  location            = azurerm_resource_group.rg_name.location
-  resource_group_name = azurerm_resource_group.rg_name.name
-  address_space       = var.address_space 
+  location            = var.location
+  resource_group_name = var.rg_name
+  address_space       = var.address_space
 
 }
 
 resource "azurerm_subnet" "medha_subnet_public" {
   name                 = var.public_subnet_name #"example-subnet"
-  resource_group_name  = azurerm_resource_group.rg_name.name
+  resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.medha_network.name
-  address_prefixes     = var.public_subnet_address_prefixes 
+  address_prefixes     = var.public_subnet_address_prefixes
 }
 
 resource "azurerm_subnet" "medha_subnet_private" {
   name                 = var.private_subnet_name #"example-subnet"
-  resource_group_name  = azurerm_resource_group.rg_name.name
+  resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.medha_network.name
   address_prefixes     = var.private_subnet_address_prefixes
 
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,22 +1,11 @@
-variable "sg_name" {
-   description = "security group for roboshop"
-}
-
-variable "network_name" {
-    type = string
-    description = "network for roboshop"
-}
-
-variable "subnet_name" {
-    type = string
-    description = "subnet for roboshop"
-}
-
-
+variable "sg_name" {}
+variable "network_name" {}
 variable "public_subnet_name" {}
-variable "private_subnet_name" {} 
-variable "address_space" {}     
-variable "private_subnet_address_prefixes" {}  
-variable "public_subnet_address_prefixes" {}  
+variable "private_subnet_name" {}
+variable "address_space" {}
+variable "private_subnet_address_prefixes" {}
+variable "public_subnet_address_prefixes" {}
 variable "network_rules" {}
 variable "private_subnet_nsg_name" {}
+variable "rg_name" {}
+variable "location" {}
