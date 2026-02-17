6f9236796a99de571217ae696d36c3d5197e334b
skommana04 <skommana04@gmail.com>
2026-02-16 18:26:15 -0600

    a

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_linux_virtual_machine" "medha-vm" {
 
     os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "StandardSSD_LRS"
   }
 
   source_image_reference {
