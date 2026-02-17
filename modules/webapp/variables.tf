f31658c308791960efc2e1a9e80901b036df7aca
skommana04 <skommana04@gmail.com>
2026-02-17 13:35:14 -0600

    a

--- modules/webapp/variables.tf
+++ modules/webapp/variables.tf
@@ -1,7 +1,7 @@
-variable web_apps {}
+
 variable "location" {}
 variable "rg_name" {}
-variable "image_tag" {}
+#variable "image_tag" {}
 variable "webapp_name" {}
 variable "acr_name" {}
 variable "image_name" {}
